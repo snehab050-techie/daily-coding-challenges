ed72eec63dd74acfa8c0660c29fb65d4989513a0
snehab050-techie <snehab050@gmail.com>
2026-07-27 12:46:59 +0530

    Code for finding vowels in a string

--- src/main/BasicQuestions/CountVowels.java
+++ src/main/BasicQuestions/CountVowels.java
@@ -1,4 +1,21 @@
 package src.main.BasicQuestions;
 
+//Program to count vowels in a String
 public class CountVowels {
+
+    static void main() {
+
+        String str = "apiple";
+        char[] c= str.toCharArray();
+        int count = 0;
+
+        for(int i=0;i<c.length;i++){
+            if(c[i] == 'a' || c[i]=='e' || c[i]=='i' || c[i] =='o' || c[i] == 'u'){
+                count++;
+            }
+        }
+        System.out.println("Vowels count in : "+str+" is : "+count);
+    }
 }
+
+
