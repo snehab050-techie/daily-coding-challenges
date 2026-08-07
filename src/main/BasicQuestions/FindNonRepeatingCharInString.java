eecde8350bc7b7e4340e14396d9d32863f785184
snehab050-techie <snehab050@gmail.com>
2026-08-07 11:06:45 +0530

    Program to Find the First non-repeating character in string using HashMap

---
+++ src/main/BasicQuestions/FindNonRepeatingCharInString.java
@@ -0,0 +1,30 @@
+package src.main.BasicQuestions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Find the First non-repeating character in string
+// HashMap technique
+public class FindNonRepeatingCharInString {
+
+    static void main() {
+        String name = "madm";
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        char[] arr = name.toCharArray();
+        for(Character c: arr){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+//        System.out.println(map);
+        char minChar;
+        int count = 1;
+        for(Map.Entry<Character,Integer> et:map.entrySet()){
+            if(et.getValue() == 1){
+                minChar = et.getKey();
+                System.out.println("The non repeating char is: "+minChar+" with count "+et.getValue());
+            }
+        }
+    }
+}
