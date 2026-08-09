6a9167638fda9acad3a37fba6d0f9e21af73e63c
snehab050-techie <snehab050@gmail.com>
2026-08-09 12:30:09 +0530

    Program to merge two arrays

--- src/main/BasicQuestions/MergeArrays.java
+++ src/main/BasicQuestions/MergeArrays.java
@@ -1,5 +1,7 @@
 package src.main.BasicQuestions;
 
+import java.util.Arrays;
+
 //Program to merge arrays
 public class MergeArrays {
     static void main() {
@@ -7,6 +9,12 @@ public class MergeArrays {
         int[] arr1 = {10,20,30,40};
         int[] arr2 = {50,60,70,80};
 
+        // Using System.arraycopy() method
+        int[] mergedArray = new int[arr1.length + arr2.length];
 
+        System.arraycopy(arr1, 0, mergedArray, 0, arr1.length);
+        System.arraycopy(arr2, 0, mergedArray, arr1.length, arr2.length);
+
+        System.out.println(Arrays.toString(mergedArray));
     }
 }
